0b9ae473cbc73517859090c83290aeb723f29fea
Will <wdoughtydev@gmail.com>
2022-04-14 21:09:27 -0500

    Updated Icons to show in icon container.

--- wdoughtydev/src/components/Navigation/NavBar.tsx
+++ wdoughtydev/src/components/Navigation/NavBar.tsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { Icon, IconContainer, NavDiv } from "../../styles/styles";
+import { Icon, IconContainer, NavDiv, NavItem } from "../../styles/styles";
+import linkedin from '../../image/icon-linkedin.png';
+import github from  '../../image/icon-github.png';
 
 
 const NavBar = () => {
     return (
         <NavDiv>
+            
             <IconContainer>
                 <a href = "https://www.linkedin.com/in/williamdoughtydev/" target="_blank" rel="noreferrer">
-                    <Icon src={'../../image/icon-linkedin.png'}></Icon>
+                    <Icon src={linkedin}></Icon>
+                    
                     
                 </a>
+            </IconContainer>
+            <IconContainer>
 
                 <a href = "https://github.com/WDoughtydev" target="_blank" rel="noreferrer">
-                    <Icon  src={`../../image/icon-github.png`} ></Icon>
+                    <Icon src={github} ></Icon>
                     
                 </a>
                 
-                
-
             </IconContainer>
 
         </NavDiv>
