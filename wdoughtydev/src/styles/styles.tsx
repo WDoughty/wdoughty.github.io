6a38dd7c2950d75292c90cb7c13e7328cf46f940
Will <wdoughtydev@gmail.com>
2022-04-14 21:09:58 -0500

    Div -> img. added in Nav item for routes to other pages.  WIP

--- wdoughtydev/src/styles/styles.tsx
+++ wdoughtydev/src/styles/styles.tsx
@@ -48,30 +48,33 @@ export const ResumeTD = styled.td`
 `
 
 export const NavDiv = styled.div`
-position: relative;
-width:100%;
-height: 60px;
-margin-left: auto;
-margin-right: auto;
-background-color: #505050;
-display:block;
-
+    position: relative;
+    width:100%;
+    height: 60px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: #505050;
+    display:block;
 `
 
+export const NavItem = styled.div`
+    postition: relative;
+    float: left;
+`
 
 
 export const IconContainer = styled.div`
     position: relative;
-    top: 8px;
     float: right;
 `
 
-export const Icon = styled.div<IconProps>`
+export const Icon = styled.img<IconProps>`
     position: relative;
     width: 44px;
     height: 44px; 
     margin-right: 8px;
     display: inline-block;
     background-repeat: no-repeat; 
-    background-image: url(${props => props.src})  
+    background-image: url(${props => props.src});
+    top: 8px; 
 `
